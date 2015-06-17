afafc2533154fcdddf7ed0740124d05b4eea66c5
Jorge López <j.l.h1989@gmail.com>
2015-06-17 17:56:34 +0200

    Solucion al arreglo del despliegue de Heroku

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/', function(req, res) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+
+//Creación de la petición al enlace 'author'
+router.get('/author', function (req, res, next) {
+ res.render('author', {});
+});
 module.exports = router;
