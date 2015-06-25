e3d2594414c6f110f26c45c80d58de4fa734e498
Jorge López <j.l.h1989@gmail.com>
2015-06-25 13:35:29 +0200

    Funciones Editar Preguntas y Borrar Preguntas.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var partials = require('express-partials');
 
+var methodOverride = require('method-override');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -24,6 +26,9 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+
+app.use(methodOverride('_method'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -137,3 +137,43 @@ exports.create = function(req,res){
 	});	
 };
 
+//GET /quizes/:id/edit
+
+exports.edit = function(req,res){
+	var quiz = req.quiz;
+	res.render('quizes/edit',{quiz:quiz, errors:[]});
+};
+
+//PUT /quizes/:id
+
+exports.update = function(req,res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz , errors: err.errors});
+			}
+
+			else{ //save: guarda campos pregunta y respuesta en DB
+				req.quiz.save({fields: ["pregunta","respuesta"]}).then(
+					function(){
+						res.redirect('/quizes');
+					});
+			} //Redireccion HTTP a lista de preguntas (URL relativo)
+		})
+}
+
+
+//DELETE /quizes/id
+exports.destroy = function(req,res){
+	req.quiz.destroy().then(function(){
+		res.redirect('/quizes');
+
+	}).catch(function(error){
+		next(error)
+	});
+};
+
+
